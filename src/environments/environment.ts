991d724ba31d5acda338043c2469f9b0745a7bd9
dalmope <eukadaju@gmail.com>
2021-11-16 10:21:16 -0500

    para Netlify

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,24 +2,25 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// // Variables for heroku
-// export const environment = {
-//   production: false,
-//   productoURL: 'https://sistemainventario-api.herokuapp.com/producto/',
-//   authURL: 'https://sistemainventario-api.herokuapp.com/auth/',
-//   changePasswordURL: 'https://sistemainventario-api.herokuapp.com/email-password/',
-//   usuariosURL: 'https://sistemainventario-api.herokuapp.com/usuarios/',
-// };
-
-// Variables for localhost
+//Variables for heroku
 export const environment = {
   production: false,
-  productoURL: 'http://localhost:8080/productos/',
-  authURL: 'http://localhost:8080/auth/',
-  changePasswordURL: 'http://localhost:8080/email-password/',
-  usuariosURL: 'http://localhost:8080/usuarios/',
-  categoriaURL: 'http://localhost:8080/categorias/',
+  productoURL: 'https://sistemainventario-api.herokuapp.com/productos/',
+  authURL: 'https://sistemainventario-api.herokuapp.com/auth/',
+  changePasswordURL: 'https://sistemainventario-api.herokuapp.com/email-password/',
+  usuariosURL: 'https://sistemainventario-api.herokuapp.com/usuarios/',
+  categoriaURL: 'https://sistemainventario-api.herokuapp.com/categorias/',
 };
+
+// //Variables for localhost
+// export const environment = {
+//   production: false,
+//   productoURL: 'http://localhost:8080/productos/',
+//   authURL: 'http://localhost:8080/auth/',
+//   changePasswordURL: 'http://localhost:8080/email-password/',
+//   usuariosURL: 'http://localhost:8080/usuarios/',
+//   categoriaURL: 'http://localhost:8080/categorias/',
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
